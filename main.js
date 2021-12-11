5466a32796e1b0b883483875701d1f248cd68655
Rishi-Bidani <64310471+Rishi-Bidani@users.noreply.github.com>
2021-12-11 12:11:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ const path = require("path");
 const { Menu } = require("electron/main");
 const { app, BrowserWindow, ipcMain, webContents } = electron;
 
-app.on("ready", function () {
-  mainWindow = new BrowserWindow({
-    width: 1100,
-  });
-  mainWindow.maximize();
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "templates/mainWindow.html"),
-      protocol: "file:",
-      slashes: true,
-    })
-  );
-  //Quit app when closed
-  mainWindow.on("closed", function () {
-    app.quit();
-  });
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({
+        width: 1100,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nativeWindowOpen: true,
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+    mainWindow.maximize();
+    mainWindow.loadFile("./templates/index.html");
+    //Quit app when closed
+    mainWindow.on("closed", function () {
+        app.quit();
+    });
 });
